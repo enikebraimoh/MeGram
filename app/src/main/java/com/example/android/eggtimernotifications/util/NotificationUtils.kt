b517a79ff42405f77c2510499f08dc8e111e3703
enikebraimoh <richardbraimoh@gmail.com>
2022-09-16 02:37:21 +0100

    work in progress ⚡️

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -29,6 +29,7 @@ import com.example.android.eggtimernotifications.receiver.SnoozeReceiver
 
 // Notification ID.
 private val NOTIFICATION_ID = 0
+private val CALLS_NOTIFICATION_ID = 1
 private val REQUEST_CODE = 0
 private val FLAGS = 0
 
@@ -64,6 +65,14 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
     // TODO: Step 2.2 add snooze action
 
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        PendingIntent.FLAG_ONE_SHOT
+    )
+
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
     var builder = NotificationCompat.Builder(
@@ -87,11 +96,39 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.1 add style to builder
 
     // TODO: Step 2.3 add snooze action
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
 
     // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
     notify(NOTIFICATION_ID, builder.build())
+}
+
+
+
+fun NotificationManager.inComingCallNotification(callerName : String, context: Context){
+
+    val eggImage = BitmapFactory.decodeResource(
+        context.resources,
+        R.drawable.cooked_egg
+    )
+
+    var builder = NotificationCompat.Builder(
+        context,
+        context.getString(R.string.calls_channel_id)
+    )
+        .setContentTitle(callerName)
+        .setContentText("Incoming Call")
+        .setLargeIcon(eggImage)
+        .setSmallIcon(R.drawable.egg_icon)
+
+
+
+    notify(CALLS_NOTIFICATION_ID,builder.build())
 
 }
 
